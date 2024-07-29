b8c0c48d29cdae3a6823725c627cf8b25267ff9d
Jamyle Elen <Jamyle Elen>
2024-07-29 13:07:50 -0300

    feat: added variable for images

---
+++ src/assets/images.js
@@ -0,0 +1,15 @@
+import carAdaptImg from './images/car-adapt.png';
+import car2AdaptImg from './images/car2-adapt.png';
+import logoAdapt from './images/logo-adapt.png';
+import peopleAdaptImg from './images/people-adapt.png';
+import moneyAdaptImg from './images/money-adapt.png';
+
+const images = {
+    carAdaptImg,
+    car2AdaptImg,
+    logoAdapt,
+    peopleAdaptImg,
+    moneyAdaptImg
+};
+
+export default images;
