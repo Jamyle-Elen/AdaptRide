a234f379243aae2572bc9adca69ecd5bd6544997
Dickson Arthur Mojica Barbosa <dicksonarthur11@gmail.com>
2024-07-29 12:07:55 -0300

    feat: added component HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import { useState } from 'react'
 import './App.css'
+import HomePage from './components/pages/homepage/homepage.jsx'
 
 function App() {
   return (
     <>
-      
+      <HomePage />
     </>
   )
 }
