7180f73488e2bdf218f53edaa430e40c63ee6434
Jamyle Elen <Jamyle Elen>
2024-08-01 19:45:59 -0300

    feat: import images added

---
+++ src/assets/images.js
@@ -0,0 +1,15 @@
+import carAdapt from './images/car-adapt.png'
+import car2Adapt from './images/car2-adapt.png'
+import logoAdapt from './images/logo-adapt.png'
+import moneyAdapt from './images/money-adapt.png'
+import peopleAdapt from './images/people-adapt.png'
+
+const images = {
+    carAdapt,
+    car2Adapt,
+    logoAdapt,
+    moneyAdapt,
+    peopleAdapt,
+}
+
+export default images;
