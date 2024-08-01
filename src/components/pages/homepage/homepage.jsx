d5aa2fce9fb049a25bbec5a8adb2121f3732a09b
Jamyle Elen <Jamyle Elen>
2024-08-01 19:49:24 -0300

    feat: components added to the home page

--- src/components/pages/homepage/homepage.jsx
+++ src/components/pages/homepage/homepage.jsx
@@ -1,9 +1,15 @@
 import react from 'react';
 import NavBar from './navbar/navbar.jsx'
+import HeroSection from './herosection.jsx';
+import AboutUsSection from './aboutussection.jsx'
+import BenefitsSection from './benefitssection.jsx'
 
 function HomePage() {
     return <>
         <NavBar/>
+        <HeroSection/>
+        <AboutUsSection/>
+        <BenefitsSection/>
     </>
 }
  
