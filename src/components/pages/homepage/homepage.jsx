bc6933ed08e51c2a67eaecad4f6bfbef3d3962a9
Dickson Arthur Mojica Barbosa <dicksonarthur11@gmail.com>
2024-08-02 19:01:34 -0300

    refactor: comment added to import and route of BenefitsSection

--- src/components/pages/homepage/homepage.jsx
+++ src/components/pages/homepage/homepage.jsx
@@ -2,14 +2,14 @@ import react from 'react';
 import NavBar from './navbar/navbar.jsx'
 import HeroSection from './herosection.jsx';
 import AboutUsSection from './aboutussection.jsx'
-import BenefitsSection from './benefitssection.jsx'
+// import BenefitsSection from './benefitssection.jsx'
 
 function HomePage() {
     return <>
         <NavBar/>
         <HeroSection/>
         <AboutUsSection/>
-        <BenefitsSection/>
+        {/* <BenefitsSection/> */}
     </>
 }
  
