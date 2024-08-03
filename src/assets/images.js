740df7cf8c5ab155b58d9408809cad6f8e19b322
David Ribeiro <davidribeiro.developer@gmail.com>
2024-08-02 22:12:04 -0300

    feat: import images added

--- src/assets/images.js
+++ src/assets/images.js
@@ -3,6 +3,9 @@ import car2Adapt from './images/car2-adapt.png'
 import logoAdapt from './images/logo-adapt.png'
 import moneyAdapt from './images/money-adapt.png'
 import peopleAdapt from './images/people-adapt.png'
+import qrcodeApp from './images/qrcode-app.png'
+import qrcodeParceiro from './images/qrcode-parceiro.png'
+import arrowAdapt from './images/arrow-Adapt.png'
 
 const images = {
     carAdapt,
@@ -10,6 +13,9 @@ const images = {
     logoAdapt,
     moneyAdapt,
     peopleAdapt,
+    qrcodeApp,
+    qrcodeParceiro,
+    arrowAdapt,
 }
 
 export default images;
