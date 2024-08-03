34f0f9a8cfd5ae188aa0a1e768b3dc49a4b2be47
Dickson Arthur Mojica Barbosa <dicksonarthur11@gmail.com>
2024-08-02 23:47:42 -0300

    feat: added import image

--- src/assets/images.js
+++ src/assets/images.js
@@ -3,6 +3,7 @@ import car2Adapt from './images/car2-adapt.png'
 import logoAdapt from './images/logo-adapt.png'
 import moneyAdapt from './images/money-adapt.png'
 import peopleAdapt from './images/people-adapt.png'
+import profileAdapt from './images/profile-adapt.png'
 
 const images = {
     carAdapt,
@@ -10,6 +11,7 @@ const images = {
     logoAdapt,
     moneyAdapt,
     peopleAdapt,
+    profileAdapt,
 }
 
 export default images;
