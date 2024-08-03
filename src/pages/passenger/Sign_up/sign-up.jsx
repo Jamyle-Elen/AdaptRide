ee8bceb271253b03b1d63fa92d9e1a2096fe2306
Dickson Arthur Mojica Barbosa <dicksonarthur11@gmail.com>
2024-08-03 01:15:02 -0300

    feat: added sign-up structure and styling

--- src/pages/passenger/Sign_up/sign-up.jsx
+++ src/pages/passenger/Sign_up/sign-up.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import images from '../../../assets/images'
+import "./sign-up.css"
 
 const SignUp = () => {
   return (
-    <div>
-      
-    </div>
+    <main className="main">
+            <section className="section">
+                <div className="sign_in_area">
+                    <img src={images.logoAdapt} alt="Logo Adapt" />
+                    <div className="sign_in_text">
+                        <h2>Olá, <span>passageiro</span>! Que bom ver você novamente.</h2>
+                        <p>Acesse sua conta</p>
+                    </div>
+                    <button className='sign_in_area_button'>Entrar</button>
+                </div>
+                <div className="sign_up">
+                    <div className="sign_up_text">
+                        <h2>Crie sua conta</h2>
+                        <p>Preencha seus dados</p>   
+                    </div>
+                    <div className="sign_up_input">
+                        <input type="text" name="" id="" placeholder="Ex.: Maria Ferreira Oliveira"/>
+                        <input type="number" name="" id="" placeholder="Ex.: 123.456.789-10" />
+                        <input type="email" name="" id="" placeholder="Ex.: mariaferreiraoliveira@gmail.com" />
+                        <input type="tel" name="" id="" placeholder="Ex.: +55 81 99999-9999"/>
+                        <input type="date" name="" id="" />
+                        <input type="password" name="" id="" placeholder="Ex.: #Maria2697V" />
+                    </div>
+                    <button>Cadastrar</button>
+                </div>
+            </section>
+        </main>
   )
 }
 
