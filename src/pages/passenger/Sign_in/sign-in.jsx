b91f006145a966344e3fdb68c44fde6064c51d7c
Dickson Arthur Mojica Barbosa <dicksonarthur11@gmail.com>
2024-08-03 01:14:40 -0300

    feat: added sign-in structure and styling

--- src/pages/passenger/Sign_in/sign-in.jsx
+++ src/pages/passenger/Sign_in/sign-in.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
-import './sign-in.css';
+import images from '../../../assets/images'
+import { Link } from 'react-router-dom'
+import './sign-in.css'
 
-function SignIn() {
+const SignIn = () => {
   return (
-    <div>
-      <p>SIGN IN</p>
-    </div>
+    <main className="main">
+        <section className="section">
+            <div className="sign-in">
+                <div className="sign-in-text">
+                    <h2>Login</h2>
+                    <p>Insira seus dados</p>   
+                </div>
+                <div className="sign-in-input">
+                    <input type="email" name="" id="" placeholder="E-mail" />
+                    <input type="password" name="" id="" placeholder="Senha" />
+                    <p>Esqueci minha senha</p>
+                </div>
+                <Link to="/profile-passenger"><button className="profile-passenger">Entrar</button></Link>
+                </div>
+                <div className="sign-up-area">
+                    <img src={images.logoAdapt} alt="Logo Adapt" />
+                    <div className="sign-up-text">
+                    <h2>Ainda não possui conta?</h2>
+                    <p>Crie sua conta</p>
+                </div>
+                <button className="sign-up-area-button">Cadastrar</button>
+            </div>
+        </section>
+    </main>
   )
 }
 
