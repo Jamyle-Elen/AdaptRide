3b5fc2ff3faf98a9440c6dd3e6857366571afb65
Dickson Arthur Mojica Barbosa <dicksonarthur11@gmail.com>
2024-08-02 23:49:11 -0300

    feat: added import and route of ProfilePassenger

--- src/routes/UI/route.jsx
+++ src/routes/UI/route.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from '../../components/pages/homepage/homepage.jsx'
 import SignIn from '../../components/pages/passenger/Sign_in/sign-in.jsx'
 import SignUp from '../../components/pages/passenger/Sign_up/sign-up.jsx'
+import ProfilePassenger from '../../components/pages/profile/profile.jsx'
 
 const Router = () => {
     return (
@@ -12,6 +13,7 @@ const Router = () => {
         <Route path='/' element={<HomePage/>}/>
         <Route path='/sign-in' element={<SignIn/>}/>
         <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/profile-passenger' element={<ProfilePassenger/>}/>
         <Route path='*' element={<h1>Not Found</h1>}/>
       </Routes>
     </BrowserRouter>
