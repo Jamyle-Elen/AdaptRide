805ef88a16ad9899f6be1adf0066d43c55409be6
Dickson Arthur Mojica Barbosa <dicksonarthur11@gmail.com>
2024-08-03 01:12:24 -0300

    feat: import image added

--- src/assets/images.js
+++ src/assets/images.js
@@ -3,6 +3,7 @@ import car2Adapt from './images/car2-adapt.png'
 import logoAdapt from './images/logo-adapt.png'
 import moneyAdapt from './images/money-adapt.png'
 import peopleAdapt from './images/people-adapt.png'
+import profileAdapt from './images/profile-adapt.png'
 import qrcodeApp from './images/QrCodeApp.png'
 import qrcodeParceiro from './images/QrCodeParceiro.png'
 import arrowAdapt from './images/Arrow-AdaptRide.png'
@@ -13,6 +14,7 @@ const images = {
     logoAdapt,
     moneyAdapt,
     peopleAdapt,
+    profileAdapt,
     qrcodeApp,
     qrcodeParceiro,
     arrowAdapt,
