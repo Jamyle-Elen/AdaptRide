bd356c3d6b15bc333957a432183c804b1948114c
Jamyle Elen <Jamyle Elen>
2024-08-06 01:31:19 -0300

    feat: routes added and placeholder

--- src/pages/passenger/Sign_in/sign-in.jsx
+++ src/pages/passenger/Sign_in/sign-in.jsx
@@ -6,28 +6,30 @@ import './sign-in.css'
 const SignIn = () => {
   return (
     <main className="main">
-        <section className="section">
-            <div className="sign-in">
-                <div className="sign-in-text">
-                    <h2>Login</h2>
-                    <p>Insira seus dados</p>   
+        <form>
+            <section className="section">
+                <div className="sign-in">
+                    <div className="sign-in-text">
+                        <h2>Login</h2>
+                        <p>Insira seus dados</p>
+                    </div>
+                    <div className="sign-in-input">
+                        <input type="email" name="" id="" placeholder="E-mail" />
+                        <input type="password" name="" id="" placeholder="Senha" />
+                        <Link to="/"><p>Esqueci minha senha</p></Link>
+                    </div>
+                    <Link to="/profile-passenger"><button className="profile-passenger">Entrar</button></Link>
+                    </div>
+                    <div className="sign-up-area">
+                        <img src={images.logoAdapt} alt="Logo Adapt" />
+                        <div className="sign-up-text">
+                        <h2>Ainda não possui conta?</h2>
+                        <p>Crie sua conta</p>
+                    </div>
+                    <Link to="/sign-up"><button className="sign-up-area-button">Cadastrar</button></Link>
                 </div>
-                <div className="sign-in-input">
-                    <input type="email" name="" id="" placeholder="E-mail" />
-                    <input type="password" name="" id="" placeholder="Senha" />
-                    <p>Esqueci minha senha</p>
-                </div>
-                <Link to="/profile-passenger"><button className="profile-passenger">Entrar</button></Link>
-                </div>
-                <div className="sign-up-area">
-                    <img src={images.logoAdapt} alt="Logo Adapt" />
-                    <div className="sign-up-text">
-                    <h2>Ainda não possui conta?</h2>
-                    <p>Crie sua conta</p>
-                </div>
-                <button className="sign-up-area-button">Cadastrar</button>
-            </div>
-        </section>
+            </section>
+        </form>
     </main>
   )
 }
--- src/pages/passenger/Sign_up/sign-up.jsx
+++ src/pages/passenger/Sign_up/sign-up.jsx
@@ -1,35 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import images from '../../../assets/images'
 import "./sign-up.css"
 
 const SignUp = () => {
   return (
     <main className="main">
+                <form>
             <section className="section">
-                <div className="sign_in_area">
-                    <img src={images.logoAdapt} alt="Logo Adapt" />
-                    <div className="sign_in_text">
-                        <h2>Olá, <span>passageiro</span>! Que bom ver você novamente.</h2>
-                        <p>Acesse sua conta</p>
+                    <div className="sign_in_area">
+                        <img src={images.logoAdapt} alt="Logo Adapt" />
+                        <div className="sign_in_text">
+                            <h2>Olá, <span>passageiro</span>! Que bom ver você novamente.</h2>
+                            <p>Acesse sua conta</p>
+                        </div>
+                        <Link to="/sign-in"><button className='sign_in_area_button'>Entrar</button></Link>
                     </div>
-                    <button className='sign_in_area_button'>Entrar</button>
-                </div>
-                <div className="sign_up">
-                    <div className="sign_up_text">
-                        <h2>Crie sua conta</h2>
-                        <p>Preencha seus dados</p>   
-                    </div>
-                    <div className="sign_up_input">
-                        <input type="text" name="" id="" placeholder="Ex.: Maria Ferreira Oliveira"/>
-                        <input type="number" name="" id="" placeholder="Ex.: 123.456.789-10" />
-                        <input type="email" name="" id="" placeholder="Ex.: mariaferreiraoliveira@gmail.com" />
-                        <input type="tel" name="" id="" placeholder="Ex.: +55 81 99999-9999"/>
-                        <input type="date" name="" id="" />
-                        <input type="password" name="" id="" placeholder="Ex.: #Maria2697V" />
-                    </div>
-                    <button>Cadastrar</button>
+                    <div className="sign_up">
+                        <div className="sign_up_text">
+                            <h2>Crie sua conta</h2>
+                            <p>Preencha seus dados</p>
+                        </div>
+                        <div className="sign_up_input">
+                            <input type="text" name="" id="" placeholder="Ex.: Maria Ferreira Oliveira"/>
+                            <input type="number" name="" id="" placeholder="Ex.: 123.456.789-10" />
+                            <input type="email" name="" id="" placeholder="Ex.: mariaferreiraoliveira@gmail.com" />
+                            <input type="tel" name="" id="" placeholder="Ex.: +55 81 99999-9999"/>
+                            <input type="date" name="" id="" placeholder="Ex.: 22/04/1500" />
+                            <input type="password" name="" id="" placeholder="Ex.: #Maria2697V" />
+                        </div>
+                        <button>Cadastrar</button>
                 </div>
             </section>
+                </form>
         </main>
   )
 }
