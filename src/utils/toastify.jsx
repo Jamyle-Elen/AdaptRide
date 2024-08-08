70c13cfb5aa69d8c48e526d037d5fad92040e8c4
Jamyle Elen <Jamyle Elen>
2024-08-08 04:33:53 -0300

    feat: file for tostify library

---
+++ src/utils/toastify.jsx
@@ -0,0 +1,15 @@
+// Lib dos alerts personalizados
+import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Toastify = () => {
+    const successfullyRegistered = () => toast("Cadastrado com sucesso!");
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default Toastify
