ae29970a8dd14ed71ac6c994c1e0d4ca39f215b2
Jamyle Elen <Jamyle Elen>
2024-08-08 04:29:35 -0300

    feat: added toastContainer

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@
 import './App.css'
 import Router from './routes/UI/route.jsx'
 import 'boxicons/css/boxicons.min.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <>
     <Router/>
+    <ToastContainer />
     </>
   )
 }
