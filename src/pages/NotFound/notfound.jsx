ec15d5689c479cf02523541f843c25aa2f5f21eb
davidrdev <davidribeiro.developer@gmail.com>
2024-08-10 01:37:05 -0300

    feat: Page NotFound

---
+++ src/pages/NotFound/notfound.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../homepage/navbar/navbar";
+import ErrorMessage from "../../components/Errormessage/errormessage";
+
+function NotFound() {
+  return (
+    <>
+      <NavBar />
+      <ErrorMessage />
+    </>
+  );
+}
+
+export default NotFound;
