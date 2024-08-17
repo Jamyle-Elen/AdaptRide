01e4fe3561253c7a23ded2a864b48af642cf1106
Jamyle Elen <Jamyle Elen>
2024-08-17 19:31:04 -0300

    feat: register drivers

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-import express from 'express'
+import  express, {urlencoded } from 'express'
 import cors from 'cors'
-import passengerRoutes from '../frontend/src/routes/passenger.routes.js'
-// import db from './config/database.js'
+import db from './config/database.js'
+import dotenv from 'dotenv'
+import Driver from './models/Driver.Model.js'
+
+dotenv.config()
 
 const app = express();
 const port = 3000;
@@ -9,7 +12,14 @@ const port = 3000;
 // Vao ser os middlewares
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: true }))  
+
+
+app.post('/drivers', (req, res) => {
+    const teste = req.body
+    const newDriver = Driver.create(teste)
+    res.status(201).send(teste)
+    console.log(teste)
+})
 
 app.listen(port, () => {
     console.log(`running in http://localhost:${port}`)
