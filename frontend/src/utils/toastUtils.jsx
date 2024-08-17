845e8eb780c71e2043f753dd78f25152421b02db
Jamyle Elen <Jamyle Elen>
2024-08-17 19:35:31 -0300

    feat: config toastify

---
+++ frontend/src/utils/toastUtils.jsx
@@ -0,0 +1,30 @@
+import { toast } from 'react-toastify'
+
+export const sucessToast = (message) => {
+    toast.success(message, {
+      autoClose: 2500,
+      hideProgressBar: true, // timer
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      className: "custom-toast-success",
+      style: {
+        backgroundColor: "#2c3f1b",
+        color: "#fff",
+    }})
+
+}
+
+export const errorToast = (message) => {
+    toast.error(message, {
+      autoClose: 2500,
+      hideProgressBar: true, // timer
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      progress: undefined,
+      theme: "dark",
+    });
+}
