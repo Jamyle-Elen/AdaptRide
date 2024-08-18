21f54507dba853254011f1fe18a4e1bd5e75ff38
Jamyle Elen <Jamyle Elen>
2024-08-18 14:45:03 -0300

    refactor: organized code

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,6 @@
-import  express, {urlencoded } from 'express'
-import cors from 'cors'
-import db from './config/database.js'
-import dotenv from 'dotenv'
-import Driver from './models/Driver.Model.js'
-
-dotenv.config()
+import express, { urlencoded } from "express";
+import cors from "cors";
+import routes from './routes/routes.js'
 
 const app = express();
 const port = 3000;
@@ -13,33 +9,10 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-
-app.post('/drivers', (req, res) => {
-    const teste = req.body
-    const newDriver = Driver.create(teste)
-    res.status(201).send(teste)
-    console.log(teste)
-})
-
-app.post('/login', async (req, res) => {
-
-    try {
-        const loginDriver = await Driver.findAll({
-            where: {
-                email: req.body.email,
-                password: req.body.password
-            }
-        });
-        console.log(loginDriver);
-        res.send(loginDriver);
-    } catch (error) {
-        res.status(500).send('Erro ao consultar o banco de dados');
-    }
-});
-
+app.use('/', routes)
 
 app.listen(port, () => {
-    console.log(`running in http://localhost:${port}`)
-})
+  console.log(`running in http://localhost:${port}`);
+});
 
-export default app
+export default app;
