531b409d47855450504455dd9a36909963a47f74
Jamyle Elen <Jamyle Elen>
2024-08-17 22:54:03 -0300

    feat: config db

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,17 +3,28 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql'
 })
 
-sequelize.authenticate()
+const syncDatabase = async() => {
+        try {
+            await db.sync();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+db.authenticate()
     .then(() => {
+        syncDatabase()
         console.log('Database, OK!')
     })
     .catch((error) => {
         console.error('Error Error Error Error, Database')
     })
 
-export default sequelize
+    
+    
+export default db
