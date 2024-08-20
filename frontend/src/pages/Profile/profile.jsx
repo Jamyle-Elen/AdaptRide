e1d269da011e8ac6477878be2c2d7fdf4cd9f62d
gabriella-almeida <gabriella14almeida@gmail.com>
2024-08-20 00:49:28 -0300

    fix: structure profile

--- frontend/src/pages/Profile/profile.jsx
+++ frontend/src/pages/Profile/profile.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'; 
+import { useParams } from 'react-router-dom';
 import images from "../../assets/images";
 import "./profile.css";
 import api from '../../../config/axios.jsx'
@@ -7,52 +8,68 @@ import SignUpDriver from "../driver/Sign_up/sign-up-driver";
 
 
 const ProfilePassenger = () => {
+    const { id } = useParams();
     const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true); // Adicionar estado de carregamento
+    const [loading, setLoading] = useState(true); 
     const [error, setError] = useState(null); 
     useEffect(() => {
-        // Função assíncrona para buscar os dados
         const fetchUserData = async () => {
             try {
-                const response = await api.get('/info'); // Busca os dados
-                setUser(response.data); // Atualiza o estado com os dados recebidos
+                const response = await api.get(/info/${id}); 
+                setUser(response.data); 
             } catch (error) {
-                setError("Failed to fetch user data"); // Define o estado de erro em caso de falha
+                setError("Failed to fetch user data"); 
             } finally {
-                setLoading(false); // Atualiza o estado de carregamento para false
+                setLoading(false); 
             }
         };
 
-        fetchUserData(); // Chama a função para buscar os dados
-    }, []);
+        fetchUserData(); 
+    }, [id]);
     
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error}</p>;
-    if (!user) return <p>No user data available</p>;
+    if (loading) return <label>Loading...</label>;
+    if (error) return <label>Error: {error}</label>;
+    if (!user) return <label>No user data available</label>;
     return (
-        <main className="profile-passenger">
-            <section className="img-profile-passenger">
-                <img src={images.profileAdapt} alt="Profile"/>
-            </section>
-            <section className="info-profile-passenger">
-                <h2>{user.name}</h2>
-                <section title="Personal Information">
-                    <p><strong>Gênero:</strong> geee</p>
-                    <p><strong>Data de nascimento:</strong> {user.date}</p>
-                    <p><strong>Phone:</strong> {user.phone}</p>
-                    <p><strong>Email:</strong> {user.email}</p>
-                </section>
-                <section title="accessibility">
-                    <p><strong>Deficiência:</strong> deficienciaa</p>
-                    <p><strong>Doença:</strong> doençaa</p>
-                    <p><strong>Alergia:</strong> alergiaaa</p>
-                </section>
-                <section title="history">
+        <main className="profile-driver">
+            <nav className="navbar">
+        
+                <Link><box-icon name='log-out' color='#ffffff' ></box-icon></Link>
+            </nav>
+            <section className="info-profile-driver">
+              
+                
+                <section className='driver'> 
+                <figure>
+                    <img src={images.profileAdapt} alt="Profile"/>
+                </figure>
 
+                </section>
+
+                <section className="personal-info" title="Informação Pessoal">
+                    <label><strong className='dados'>Dados Pessoais</strong></label>
+                    <label><strong>Name:</strong>{user.name}</label>
+                    <label><strong>Cpf:</strong> {user.cpf}</label>
+                    <label><strong>Email:</strong> {user.email}</label>
+                    <label><strong>Telefone:</strong> {user.phone}</label>
+                    <label><strong>Data de Nascimento:</strong> {user.dateBirth}</label>
+                    <label><strong>Número CNH:</strong> {user.numCNH}</label>
+                </section>
+                <section className="vehicle-info" title="Informação do veículo">
+                    <div className='title'>
+                        <label><strong className='title-veiculo'>Informações do veículo</strong></label>
+                    </div>
+                    <label><strong>Placa do veículo:</strong> {user.vehiclePlate}</label>
+                    <label><strong>Marca do veículo:</strong> {user.vehicleBrand}</label> 
+                    <label><strong>Modelo do veículo:</strong> {user.vehicleModel}</label> 
+                    <label><strong>Cor do veículo:</strong> {user.vehicleColor}</label>
+                    <label><strong>Tipos de adaptações:</strong> {user.typesAdaptations}</label>
+                    <label><strong>Capacidade:</strong> {user.totalCapacity}</label> 
+                    <label><strong>Descrição de adaptações:</strong> {user.descriptionAdaptations}</label>
                 </section>
             </section>
         </main>
     );
 };
 
-export default ProfilePassenger;
+export default ProfileDriver;
