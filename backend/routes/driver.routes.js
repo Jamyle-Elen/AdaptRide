5eb5fe098ef8e6a450e2bafa6355a073de8a83cd
gabriella-almeida <gabriella14almeida@gmail.com>
2024-08-20 00:34:19 -0300

    fix: added router info

--- backend/routes/driver.routes.js
+++ backend/routes/driver.routes.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import { createDrivers, loginDriver } from '../controllers/driverController.js'
+import { createDrivers, loginDriver, infoDriver } from '../controllers/driverController.js'
 
 const router = express.Router()
 
 router.post('/drivers', createDrivers)
 router.post('/login', loginDriver)
-app.get('/info',infoDriver)
+router.get('/info',infoDriver)
 
 export default router
