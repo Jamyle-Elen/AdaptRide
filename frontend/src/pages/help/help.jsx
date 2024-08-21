9869c177799c7296b141fe66400376b856ec8c25
David Ribeiro <davidribeiro.developer@gmail.com>
2024-08-21 15:11:34 -0300

    feat: Importing the help page

---
+++ frontend/src/pages/help/help.jsx
@@ -0,0 +1,15 @@
+import NavBar from "../../components/NavBar/navbar";
+import HelpMessage from "../../components/HelpMessage/helpmessage";
+import Footer from "../../components/Footer/footer";
+
+const Help = () => {
+  return (
+    <>
+      <NavBar />
+      <HelpMessage/>
+      <Footer />
+    </>
+  );
+};
+
+export default Help;
