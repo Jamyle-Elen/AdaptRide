c09cfa7271606483af433be1a671db83183b2204
Dickson Arthur Mojica Barbosa <dicksonarthur11@gmail.com>
2024-08-22 03:03:22 -0300

    refactor: import update

--- frontend/src/pages/homepage/homepage.jsx
+++ frontend/src/pages/homepage/homepage.jsx
@@ -1,7 +1,7 @@
 import react from "react";
 import NavBar from "../../components/NavBar/navbar.jsx";
 import HeroSection from "../../components/HomeSections/HeroSection/herosection.jsx";
-import AboutUsSection from "../../components/HomeSections/AbautUsSection/AboutUsSection.jsx";
+import AboutUsSection from "../../components/HomeSections/AboutUsSection/AboutUsSection.jsx";
 import BenefitsSection from "../../components/HomeSections/BenefitsSection/BenefitsSection.jsx";
 import Footer from "../../components/Footer/footer.jsx";
 
