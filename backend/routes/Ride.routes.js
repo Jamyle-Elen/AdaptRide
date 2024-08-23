58be8c9379b65e486b305ceda7ed542e74e923ac
Jamyle Elen <Jamyle Elen>
2024-08-23 04:41:38 -0300

    feat: added router request rides

--- backend/routes/Ride.routes.js
+++ backend/routes/Ride.routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createRides, getAllRides } from '../controllers/RideController.js'
+import { createRides, getAllRides, requestRides } from '../controllers/RideController.js'
 
 const router = express.Router()
 
 router.post('/rides', createRides)
 router.get('/rides', getAllRides)
+router.post('/request-rides', requestRides)
 
 export default router
