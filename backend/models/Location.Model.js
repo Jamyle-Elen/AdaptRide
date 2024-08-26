9c9b3266355e45e939c3b0f2d3b946cfa3cda0b4
Jamyle Elen <Jamyle Elen>
2024-08-26 02:02:48 -0300

    refactor: import

---
+++ backend/models/Location.Model.js
@@ -0,0 +1,19 @@
+import db from "../config/database.js";
+import { DataTypes } from "sequelize";
+
+const Location = db.define(
+  "Location",
+  {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default Location;
