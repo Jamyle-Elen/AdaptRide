a89d21a2cd42293166c74f6ac80ff444b3f8c41f
Jamyle Elen <Jamyle Elen>
2024-08-26 02:08:02 -0300

    feat: commented on some routes

--- backend/routes/Ride.routes.js
+++ backend/routes/Ride.routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { createRides, getAllRides, requestRides } from '../controllers/RideController.js'
+import { /*createRides*/ getAllRides, requestRides, acceptRide } from '../controllers/RideController.js'
 
 const router = express.Router()
 
-router.post('/rides', createRides)
+// router.post('/rides', createRides)
 router.get('/rides', getAllRides)
 router.post('/request-rides', requestRides)
+router.post('/accept-ride', acceptRide)
 
 export default router
