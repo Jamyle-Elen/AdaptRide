d93ff2be952786e15302cf3e09639ece9095ea78
Dickson Arthur Mojica Barbosa <dicksonarthur11@gmail.com>
2024-08-26 22:58:57 -0300

    refactor: "safealert" componentization

--- frontend/src/pages/SafeAlert/index.jsx
+++ frontend/src/pages/SafeAlert/index.jsx
@@ -1,49 +1,15 @@
-import { GoogleMap, useJsApiLoader, CircleF } from "@react-google-maps/api"
-
-import "./index.css"
+import NavBar from "../../components/NavBar/navbar"
+import MapAlert from "../../components/MapAlert"
+import Footer from "../../components/Footer/footer"
 
 const SafeAlert = () => {
 
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    gloogeMapsApiKey: "AIzaSyDxQYu3wlZJG8HCWI1wqjA0QUDe28_W4UE",
-  })
-
-  const position = {
-    lat: -8.082321, 
-    lng: -34.985314,
-  }
-
   return (
-    // Forma para aplicar uma marcação geométrica em uma região do mapa
-    <section className="safe-area">
-      <div className="safe-text">
-        <h1>Aqui será exibido o mapa</h1>
-        <p>Algumas observações e cuidados</p>
-      </div>
-      <div className="safe-map" id="safe-map">
-        { isLoaded ? (
-          <GoogleMap 
-            mapContainerStyle={{ width: "70vw", height: "50vh" }}
-            center={position}
-            zoom={15}
-          >
-          <CircleF 
-            center={position}
-            radius={500}
-            options={{
-              strokeColor: '#FF0000',
-              strokeWeight: 1,
-              strokeOpacity: 1,
-              fillColor: '#FF0000',
-              fillOpacity: 0.2,
-            }}
-          />
-          </GoogleMap>
-          ) : <></>
-        }
-      </div>
-    </section>
+    <>
+      <NavBar />
+      <MapAlert />
+      <Footer />
+    </>
   )
 }
 
