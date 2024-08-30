df988db6cdead14c5916ac7b3f32811133583b78
gabriella-almeida <gabriella14almeida@gmail.com>
2024-08-30 02:29:59 -0300

    feat: sideBar component

--- frontend/src/components/sideBar/sideBar.jsx
+++ frontend/src/components/sideBar/sideBar.jsx
@@ -11,10 +11,12 @@ const SideBar = () => {
                 <Link to="/history"><abbr title="Histórico"><i className='bx bx-time-five'></i></abbr></Link>
                 <Link to="/help"><abbr title="Central de Atendimento"><i className='bx bx-user-voice'></i></abbr></Link>
                 <Link to="/exit"><abbr title="Sair"><i className='bx bx-log-out'></i></abbr></Link>
-                
+                <Link to="/historyPassenger"><abbr title="Histórico"><i className='bx bx-time-five'></i></abbr></Link>
+            
             </nav>
         </div>
     );
 }
 
 export default SideBar;
+
