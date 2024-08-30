d54ef5f5f90c3e55b89bcf928b67eb37af9b4c96
gabriella-almeida <gabriella14almeida@gmail.com>
2024-08-30 02:34:19 -0300

    refactor: changed image by inserting the navbar component

--- frontend/src/pages/about/about.jsx
+++ frontend/src/pages/about/about.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import "./about.css";
 import images from "../../assets/images";
+import NavBar from "../../components/NavBar/navbar.jsx";
+
 const AboutPage = () => {
   return (
+    <>
+    <NavBar />
     <main className="aboutPage">
       <section className="page">
-        <div className="aboutPage_text1">
+        <div className="aboutPage_text">
           <h2>Quem somos nós?</h2>
           <p>
             Somos o AdaptRide que está comprometido em ajudar e melhorar a
             qualidade de vida de brasileiros portadores de deficiência.
           </p>
         </div>
-        
-        <div className="aboutPage_text2">
+        <div className="aboutPage_img">
+          <img src={images.about} alt="about image" />
+          <img className="aboutPage_img2" src={images.logoAdapt2} alt="about image2" />
+        </div>
+
+        <div className="aboutPage_text">
           <h2>O que fazemos?</h2>
           <p>
             Nosso objetivo é promover acessibilidade e oferecer às pessoas com
@@ -26,15 +34,9 @@ const AboutPage = () => {
           </p>
         </div>
       </section>
-      <section>
-        <div className="aboutPage_img">
-          <img src={images.about} alt="about image" />
-        </div>
-      </section> 
     </main>
+    </>
   );
 };
 
 export default AboutPage;
-
-
