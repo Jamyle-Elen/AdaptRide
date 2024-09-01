45a14496e521dc941d660dcd614b616a6469833a
Jamyle Elen <Jamyle Elen>
2024-09-01 10:15:51 -0300

    refactor: added path to dashboard

--- frontend/src/components/sideBar/sideBar.jsx
+++ frontend/src/components/sideBar/sideBar.jsx
@@ -7,7 +7,7 @@ const SideBar = () => {
         <div className="sidebar-container">
             <nav className="sidebar">
                 <Link to="/"><abbr title="Início"><i className='bx bx-home-alt-2'></i></abbr></Link>
-                <Link to="/trip"><abbr title="Corrida"><i className='bx bx-trip'></i></abbr></Link>
+                <Link to="/dashboard/driver"><abbr title="Corrida"><i className='bx bx-trip'></i></abbr></Link>
                 <Link to="/history"><abbr title="Histórico"><i className='bx bx-time-five'></i></abbr></Link>
                 <Link to="/help"><abbr title="Central de Atendimento"><i className='bx bx-user-voice'></i></abbr></Link>
                 <Link to="/exit"><abbr title="Sair"><i className='bx bx-log-out'></i></abbr></Link>
