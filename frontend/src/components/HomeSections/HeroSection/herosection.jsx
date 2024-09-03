867e32d7f6251c68bb96559a69efe331de396166
David Ribeiro <davidribeiro.developer@gmail.com>
2024-09-03 02:28:35 -0300

    feat: responsiveness

--- frontend/src/components/HomeSections/HeroSection/herosection.jsx
+++ frontend/src/components/HomeSections/HeroSection/herosection.jsx
@@ -148,7 +148,7 @@ const HeroSection = () => {
                 />
                 <i
                   className={`bx ${geolocationActive ? "bx-x" : "bx-target-lock"}`}
-                  style={{ color: "#ffffff", cursor: "pointer" }}
+                  style={{ color: "#3D4A6A", cursor: "pointer" }}
                   onClick={geolocationActive ? handleClearLocation : handleGeolocation}
                 ></i>
               </div>
@@ -165,10 +165,6 @@ const HeroSection = () => {
                   placeholder="Para onde você quer ir?"
                   required
                 />
-                <i
-                  className="bx bx-target-lock"
-                  style={{ color: "#ffffff", cursor: "pointer" }}
-                ></i>
               </div>
               <button type="submit">Solicitar</button>
             </form>
